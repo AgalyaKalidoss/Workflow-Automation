72eda452b74a1e13bc8bd4c3c0226836a47e7b2d
Agalya Kalidoss <agalyakalidoss02@gmail.com>
2026-03-20 12:31:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ const __dirname = path.dirname(__filename);
 async function startServer() {
   try {
     // ✅ MongoDB Connection
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected");
 
